eec1803925c1948342ca8c769699064287c49340
Ryan Whittaker <ryanjameswhittaker@gmail.com>
2018-10-17 11:32:49 -0700

    Switched from maknz to alek13

--- src/ServiceProviderLaravel5.php
+++ src/ServiceProviderLaravel5.php
@@ -26,7 +26,7 @@ class ServiceProviderLaravel5 extends \Illuminate\Support\ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/config/config.php', 'slack');
 
-        $this->app->singleton('maknz.slack',function ($app) {
+        $this->app->singleton('maknz.slack', function ($app) {
             return new Client(
                 $app['config']->get('slack.endpoint'),
                 [
